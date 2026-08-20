30706511f7332b113119d31942c401c891d09363
Akajay35 <akajay35@gmail.com>
2026-08-20 20:26:37 +0530

    fix(android): enforce JVM 17 for all Java compile tasks

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,12 @@
+import org.gradle.api.tasks.compile.JavaCompile
+
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+}
+
+// Enforce Java 17 for every JavaCompile task in the Android project.
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
 }
